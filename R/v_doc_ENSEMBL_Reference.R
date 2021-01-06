ca21463ac75a50b1cfad2da4c54359bed8f606e6
yilixu <31497432+yilixu@users.noreply.github.com>
2021-01-05 21:39:37 -0800

    Updating document

--- R/v_doc_ENSEMBL_Reference.R
+++ R/v_doc_ENSEMBL_Reference.R
@@ -1,38 +1,38 @@
-#' Information of 38,965 genes with ENSEMBL ID (build GRCh38/hg38).
+#' Information on 38,965 genes with ENSEMBL ID (build GRCh38/hg38).
 #'
-#' A reference dataset containing the information of 38,965 genes with ENSEMBL ID (build GRCh38/hg38) for vigilante.knights (the main package).
+#' A reference dataset containing the information on 38,965 genes with ENSEMBL ID (build GRCh38/hg38) for vigilante.knights (the main package).
 #'
 #' @format A list with 3 elements:
 #' \describe{
-#'   \item{GRCh38G}{dataframe, information of genes}
-#'   \item{chr_len_H}{named integer vector, information of chromosomes}
-#'   \item{GRCh38T}{dataframe, information of transcripts}
+#'   \item{GRCh38G}{dataframe, information on genes}
+#'   \item{chr_len_H}{named integer vector, information on chromosomes}
+#'   \item{GRCh38T}{dataframe, information on transcripts}
 #' }
 #' @source \url{http://www.ensembl.org/}
 "GRCh38_ref"
 
-#' Information of 33,579 genes with ENSEMBL ID (build GRCh37/hg19).
+#' Information on 33,579 genes with ENSEMBL ID (build GRCh37/hg19).
 #'
-#' A reference dataset containing the information of 33,579 genes with ENSEMBL ID (build GRCh37/hg19) for vigilante.knights (the main package).
+#' A reference dataset containing the information on 33,579 genes with ENSEMBL ID (build GRCh37/hg19) for vigilante.knights (the main package).
 #'
 #' @format A list with 3 elements:
 #' \describe{
-#'   \item{GRCh37G}{dataframe, information of genes}
-#'   \item{chr_len_H}{named integer vector, information of chromosomes}
-#'   \item{GRCh37T}{dataframe, information of transcripts}
+#'   \item{GRCh37G}{dataframe, information on genes}
+#'   \item{chr_len_H}{named integer vector, information on chromosomes}
+#'   \item{GRCh37T}{dataframe, information on transcripts}
 #' }
 #' @source \url{http://www.ensembl.org/}
 "GRCh37_ref"
 
-#' Information of 55,435 genes with ENSEMBL ID (build GRCm38/mm10).
+#' Information on 55,435 genes with ENSEMBL ID (build GRCm38/mm10).
 #'
-#' A reference dataset containing the information of 55,435 genes with ENSEMBL ID (build GRCm38/mm10) for vigilante.knights (the main package).
+#' A reference dataset containing the information on 55,435 genes with ENSEMBL ID (build GRCm38/mm10) for vigilante.knights (the main package).
 #'
 #' @format A list with 3 elements:
 #' \describe{
-#'   \item{GRCm38G}{dataframe, information of genes}
-#'   \item{chr_len_M}{named integer vector, information of chromosomes}
-#'   \item{GRCm38T}{dataframe, information of transcripts}
+#'   \item{GRCm38G}{dataframe, information on genes}
+#'   \item{chr_len_M}{named integer vector, information on chromosomes}
+#'   \item{GRCm38T}{dataframe, information on transcripts}
 #' }
 #' @source \url{http://www.ensembl.org/}
 "GRCm38_ref"
